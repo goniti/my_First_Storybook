a7209856fbfe177ca1c09acba44ef3d08f099dd4
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-21 19:56:50 +0200

    adding dynamic data, setting up the weather widget proptypes

--- canwego/src/components/MainWeather/MainWeather.js
+++ canwego/src/components/MainWeather/MainWeather.js
@@ -3,11 +3,11 @@ import WeatherOverview from "../WeatherOverview/WeatherOverview";
 import WeatherWidget from "../WeatherWidget/WeatherWidget";
 
 const MainWeather = () => {
-  console.log(process.env.OPEN_WEATHER_API_KEY);
   const [data, setData] = useState([]);
+  const isLoading = data.length !== 0;
   useEffect(() => {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=issoudun&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=issoudun&units=metric&appid=${process.env.REACT_APP_API_KEY}`
     )
       .then((response) => response.json())
       .then(
@@ -21,10 +21,14 @@ const MainWeather = () => {
   }, []);
 
   return (
-    <main>
-      <WeatherOverview degree={27} />
-      <WeatherWidget weatherData={data} />
-    </main>
+    <>
+      {isLoading && (
+        <main>
+          <WeatherOverview degree={parseInt(data.main.temp)} />
+          <WeatherWidget weatherData={data} />
+        </main>
+      )}
+    </>
   );
 };
 
--- canwego/src/components/WeatherWidget/WeatherWidget.js
+++ canwego/src/components/WeatherWidget/WeatherWidget.js
@@ -3,28 +3,73 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const WeatherWidget = ({ weatherData }) => {
-  console.log(weatherData)
-return (
- 
-  
-  <WidgetWrapper>
-    <WidgetWeatherTopPart>
-      <TopItemLeft>
-        <TopItemCurrentDate>{Date.now()}</TopItemCurrentDate>
-        <TopItemIllustration src="http://openweathermap.org/img/wn/04d@2x.png" />
-      </TopItemLeft>
-      <TopItemRight>
-        <TopItemMinDegree>temp_min</TopItemMinDegree>
-        <TopItemMaxDegree>temp_max</TopItemMaxDegree>
-      </TopItemRight>
-    </WidgetWeatherTopPart>
-    <WidgetWeatherBottomPart>
-      <BottomItem />
-      <BottomItemImage />
-      <BottomItemText />
-    </WidgetWeatherBottomPart>
-  </WidgetWrapper>
-)
+  return (
+    <WidgetWrapper>
+      <WidgetWeatherTopPart>
+        <TopItemLeft>
+          <TopItemCurrentDate>{weatherData.dt}</TopItemCurrentDate>
+          <TopItemIllustration
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          />
+        </TopItemLeft>
+        <TopItemRight>
+          <TopItemMinDegree>{weatherData.main.temp_min}</TopItemMinDegree>
+          <TopItemMaxDegree>{weatherData.main.temp_max}</TopItemMaxDegree>
+        </TopItemRight>
+      </WidgetWeatherTopPart>
+      <WidgetWeatherBottomPart>
+        <BottomItem />
+        <BottomItemImage />
+        <BottomItemText />
+      </WidgetWeatherBottomPart>
+    </WidgetWrapper>
+  );
+};
+
+WeatherWidget.propTypes = {
+  weatherData: PropTypes.shape({
+    base: PropTypes.string.isRequired,
+    clouds: PropTypes.shape({
+      all: PropTypes.number.isRequired,
+    }).isRequired,
+    cod: PropTypes.number.isRequired,
+    coord: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lon: PropTypes.number.isRequired,
+    }).isRequired,
+    dt: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    main: PropTypes.shape({
+      feels_like: PropTypes.number.isRequired,
+      humidity: PropTypes.number.isRequired,
+      pressure: PropTypes.number.isRequired,
+      temp: PropTypes.number.isRequired,
+      temp_max: PropTypes.number.isRequired,
+      temp_min: PropTypes.number.isRequired,
+    }).isRequired,
+    name: PropTypes.string.isRequired,
+    sys: PropTypes.shape({
+      country: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      sunrise: PropTypes.number.isRequired,
+      sunset: PropTypes.number.isRequired,
+      type: PropTypes.number.isRequired,
+    }).isRequired,
+    timezone: PropTypes.number.isRequired,
+    visibility: PropTypes.number.isRequired,
+    weather: PropTypes.arrayOf(
+      PropTypes.shape({
+        description: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        main: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    wind: PropTypes.shape({
+      deg: PropTypes.number.isRequired,
+      speed: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default WeatherWidget;
